336c66534b52a4c03ffc5b6af8c13b2e253775eb
Abderlahman Salem <abdelrahmansalemfahmy@gmail.com>
2024-07-19 19:12:42 +0300

    files

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 import * as net from "net";
+import * as fs from "fs";
+
+const args = process.argv.slice(2);
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -31,6 +34,24 @@ const server = net.createServer((socket) => {
             `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
           )
         );
+      } else if (
+        url.startsWith("/files/") &&
+        !url.endsWith("/files/") &&
+        args[0] === "--directory"
+      ) {
+        const dir = args[1];
+        const file = url.split("/")[2];
+        const filePath = dir + file;
+        fs.readFile(filePath, "utf8", (err, data) => {
+          if (err) socket.write(Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n"));
+          else {
+            socket.write(
+              Buffer.from(
+                `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`
+              )
+            );
+          }
+        });
       } else socket.write(Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n"));
     }
   });
